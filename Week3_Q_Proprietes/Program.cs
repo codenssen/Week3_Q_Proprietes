d824bafcddc2c1223d23ec31a058939375369865
ABT Aurelien <aurelien@abrassart.net>
2024-06-19 13:39:16 +0200

    Ajoutez des fichiers projet.

---
+++ Week3_Q_Proprietes/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Encapsulation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            House house = new House();
+            Room room = new Room();
+            Door houseDoor = new Door();
+            Door roomDoor = new Door();
+            house.AddDoor(houseDoor);
+            room.AddDoor(roomDoor);
+            house.AddRoom(room);
+        }
+    }
+
+    public class House
+    {
+        private List<Room> _rooms;
+        public List<Door> _doors;
+
+        public House()
+        {
+            _rooms = new List<Room>();
+            _doors = new List<Door>();
+        }
+
+        public List<Room> Rooms
+        {
+            get { return _rooms; }
+            set
+            {
+                _rooms = _rooms.Add(value);
+            }
+        }
+
+        public void AddDoor(Door door)
+        {
+            _doors.Add(door);
+        }
+
+        public void AddRoom(Room room)
+        {
+            _rooms.Add(room);
+        }
+    }
+
+    public class Room
+    {
+        public List<Door> _doors;
+
+        public Room()
+        {
+            _doors = new List<Door>();
+        }
+
+        public void AddDoor(Door door)
+        {
+            _doors.Add(door);
+        }
+    }
+
+    public class Door
+    {
+        public bool _isOpen;
+
+        public Door()
+        {
+            _isOpen = false;
+        }
+
+        public void Open()
+        {
+            if (_isOpen)
+            {
+                Console.WriteLine("Door already opened. Ain't done anything.");
+            }
+            else
+            {
+                _isOpen = true;
+            }
+        }
+
+        public void Close()
+        {
+            if (!_isOpen)
+            {
+                Console.WriteLine("Door already closed. Ain't done anything.");
+            }
+            else
+            {
+                _isOpen = false;
+            }
+        }
+    }
+}
